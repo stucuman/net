2edae7af3a4c109bf252ea8c08578e15f015a9cd
Seba Gomes <sebastian.gomes@gmail.com>
2021-05-26 14:32:43 -0300

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,59 @@ const Home = () => (
     </h1>
 
     <h3>
-      Hooray 🎉 - you've built this with{' '}
-      <a href="https://nextjs.org">Next.js</a>!
+      <a href="http://lacritica.com.ar">noticias de tucuman</a>  
+ <a href="https://lacritica.com.ar/clima-en-tucuman/">Clima en Tucuman</a>  
+ ceferino decima  
+ <a href="https://lacritica.com.ar/2020/02/03/ana-pedraza-la-periodista-tucumana-que-cautiva-con-su-carisma-y-aman-en-buenos-aires/"> ana pedraza</a>  
+ Celia Nahra  
+ juan manzur  
+ osvaldo jaldo  
+ jose cano  
+ silvia elias de perez  
+ jose maria canelada  
+ <a href="https://lacritica.com.ar/clima-en-tucuman/">clima en Tucuman</a>  
+ <a href="https://lacritica.com.ar/quiniela-de-tucuman"><strong>quiniela de tucuman</strong></a>  
+ <a href="https://lacritica.com.ar/2020/04/27/es-la-senora-poron-el-tucu-lopez-mostro-de-mas-y-ardio-twitter/">tucu lopez</a>  
+ <a class="" href="https://lacritica.com.ar/2020/01/27/silvina-saleme-posleman-le-prendio-fuego-a-los-cerros-tucumanos-con-fotos-super-hot/">silvina saleme posleman</a>  
+ <a href="https://lacritica.com.ar/2020/02/19/jorgelina-zalazar-vs-gladys-la-bomba-tucumana-que-susto-que-se-llevo-la-video/?swcfpc=1">jorgelina zalazar</a>  
+ pablo verxelitaki  
+ daniela suarez  
+ <a href="https://lacritica.com.ar/?s=daniela+orce%C2%A0">daniela orce</a>  
+ <a href="https://lacritica.com.ar/?s=lucia+rossi%C2%A0">lucia rossi</a>  
+ <a href="http://argencon.org">Argencon</a>  
+ <a href="http://telefonosatencioncliente.xyz">Telefonos de atencion al cliente</a>  
+ <a href="https://lacritica.netlify.app">netlify</a>  
+ <a href="https://sites.google.com/view/tuquman/">tucuman  
+ <a href="https://sebastiangomes5.wixsite.com/tuquman">tucuman  
+ <a href="https://proyectoasistir.com.ar">piscologo online</a>  
+ <a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a>  
+ <a href="https://proyectoasistir.com.ar">piscologo online </a>  
+ <a href="https://sastreriagiordano.com.ar">alquiler de trajes para fiestas en tucuman</a>  
+ <a href="https://sastreriagiordano.com.ar">alquiler de trajes en tucuman</a>  
+ <a href="https://quepicada.com.ar">picadas en Tucuman</a>  
+ <a href="https://quepicada.com.ar">delivery de picadas en Tucuman</a>  
+ <a href="https://quepicada.com.ar">picadas a domicilio en Tucuman</a>  
+ <a href="https://quepicada.com.ar">picadas para regalar en Tucuman</a>  
+ <a href="https://quepicada.com.ar">regalos empresariales en Tucuman</a>  
+ <a href="https://quepicada.com.ar">box para regalar en tucuman</a>  
+ <a href="https://images.google.de/url?q=https://lacritica.com.ar/">noticias de tucuman</a>
+ <a href="https://images.google.co.uk/url?q=https://lacritica.com.ar/">noticias de tucuman</a>
+ <a href="https://images.google.co.jp/url?q=https://lacritica.com.ar/">tucuman</a>
+ <a href="https://images.google.co.jp/url?q=https://proyectoasistir.com.ar">piscologo online</a>  
+ <a href="https://images.google.es/url?q=https://lacritica.com.ar/">ultimo momento tucuman</a>
+ <a href="https://www.ted.com/profiles/28003350">TED  
+ <a href="https://people.mozilla.org/p/sebasg">Mozilla  
+ <a href="https://www.google.es/url?q=https://lacritica.com.ar/"> ricardo bussi </a>  
+ <a href="https://spark.adobe.com/page/QGyPFWsmiJCTK/">adobe </a>  
+ <a href="marketingdigitalexperience.com/curso-de-marketing-digital/">curso de marketing digital </a>  
+ <a href="https://huffduffer.com/sebasg/610279">huffduffer  
+ <a href="https://spacebabe.ams3.digitaloceanspaces.com/index.html">Spaces babe</a>  
+ <a href="https://epaepa.oss-ap-northeast-1.aliyuncs.com/index.html">Epaepadiscoestepa</a>  
+ <a href="https://bit.ly/3eLLr0v">bitly</a>  
+ <a href="https://tucuman.hatenablog.com/entry/2021/05/15/133138">hatena</a>  
+ <a href="https://lacritica.com.ar/2021/05/16/barbie-beltramino/">Barbie Beltramino</a>  
+ <a href="https://ar.pinterest.com/LACRITICAok">pinterest</a>  
+
     </h3>
 
     <style jsx>{`
